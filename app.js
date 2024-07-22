581db0c58b1292be261a93b0b32d8a184609c71e
DeepakDibyajyoti <deepak.dibyajyoti@cybernetyx.com>
2024-07-23 02:10:36 +0530

    fix: change app.js file

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var app = express();
 app.use(cors());
 require("dotenv").config();
 require("./dbconfig");
+app.use(express.json());
 const productModel = require("./model/product");
 app.post("/products", async function (req, res, next) {
-  const product = new productModel({ name: "p01", price: 2343 });
+  const { name, price } = req.body;
+  const product = new productModel({ name: name, price: price });
   const newProduct = await product.save();
   return res.status(201).json({
     message: "product created",
